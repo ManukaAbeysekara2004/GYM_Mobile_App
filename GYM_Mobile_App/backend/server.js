197af3d34241a87f6ff5a7b4e0c1190cb0826e22
Manuka Abeysekara <it24100043@sliit.lk>
2026-06-10 20:55:29 +0530

    finish admin management functionality

--- GYM_Mobile_App/backend/server.js
+++ GYM_Mobile_App/backend/server.js
@@ -22,6 +22,7 @@ app.use('/api/user', require('./routes/UserRoutes'));
 app.use('/api/admin', require('./routes/AdminRoutes'));
 app.use('/api/gym', require('./routes/GymRoutes'));
 app.use('/api/coach', require('./routes/CoachRoutes'));
+app.use('/api/admin-manage', require('./routes/AdminManageRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
